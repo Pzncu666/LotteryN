e2cb58f7d77a61c1e32b568c22e6afa124e7f1e4
LAPTOP-0LOM1CM3\mimi <ddcc2100@gmail.com>
2018-04-27 13:28:53 +0800

    能夠輸出10組

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            //更變顏色
+
+            Random rand = new Random();
+            //產生新變數
+
+            int i = 0;
+            while (i < 10)
+            {
+                int r1 = rand.Next(0, 43);
+                int r2 = rand.Next(0, 43);
+                int r3 = rand.Next(0, 43);
+                int r4 = rand.Next(0, 43);
+                int r5 = rand.Next(0, 43);
+                int r6 = rand.Next(0, 43);
+                Console.Write("號碼{0:00},{1:00},{2:00},{3:00},{4:00},{5:00}", r1, r2, r3, r4, r5, r6);
+                i++;
+            }
+            //輸出變數
+
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
